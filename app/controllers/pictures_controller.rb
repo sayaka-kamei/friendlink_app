82d5e9c255ec237da45284fb89f244de2cc491d7
sayaka kamei <sayukou316@gmail.com>
2022-10-08 17:20:23 +0900

    privateメソッドを作成した

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,14 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
   def create
-    Picture.create(image: params[:picture][:image], content: params[:picture][:content], user: params[:picture][:user])
-    redirect_to "/pictures/new"
+    Picture.create(picture_params)
+    redirect_to new_picture_path
   end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/pictures', to: 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures
