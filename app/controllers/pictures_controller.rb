cab3cac04f6bb1c1d27d0340a4b73caf178fa762
sayaka kamei <sayukou316@gmail.com>
2022-10-08 19:38:41 +0900

    投稿削除機能を作成した

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
   def index
     @pictures = Picture.all
   end
@@ -15,25 +17,32 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
   end
 
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿を編集しました！"
     else
       render :edit
     end
   end
+
+  def destroy
+    @blog.destroy
+    redirect_to pictures_path, notice:"投稿を削除しました！"
+  end
+
   private
 
   def picture_params
     params.require(:picture).permit(:image, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 
 end
